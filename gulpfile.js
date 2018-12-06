adb22854828bd97fb817777a20c3136b1bce9fc0
ER <zhongzsp@aliyun.com>
2018-12-06 17:47:30 +0800

    add test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
 var less = require('gulp-less')
 var plumber = require('gulp-plumber')
+const gulpWebpack = require('gulp-webpack')
 gulp.task('watch', function() {
     gulp.watch("./less/**/*", ['less'])
 });
@@ -11,5 +12,31 @@ gulp.task('less', function() {
         .pipe(less())
         .pipe(gulp.dest("./styles"))
 });
+
+//测试环境
+gulp.task('test', () => {
+  return gulp.src('./test/main.js')
+  .pipe(gulpWebpack({
+    resolve: {
+      alias: {
+        'vue': 'vue/dist/vue.js'
+      }
+    },
+    entry: ['./test/main.js'],
+    output: {
+      filename: 'test.js'
+    },
+    module: {
+     loaders: [
+       { test: /\.js$/, loader: 'babel-loader', exclude: '/node_modules' },
+       { test: /\.vue$/, loader: 'vue' },
+     ]
+    },
+    babel: {
+      presets: ['es2015']
+    }
+  }))
+  .pipe(gulp.dest("test"))
+})
 
 gulp.task('default', ['watch', 'less'])
---
+++ packages/layout/module-menu.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+   <div v-for="(item, key) in menu"
+   :key="key"
+   @click="openUrl(item.path)"
+   :class="('active': path === item.path)">
+     <i :class="item.icom"></i>
+     <div>{{item.name}}</div>
+   </div> 
+  </div>
+</template>
+
+<script>
+  props: {
+    menu: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    path: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    openUrl () {
+      
+    }
+  }
+</script>
