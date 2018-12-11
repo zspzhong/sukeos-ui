3554ca9f3d0da881e62bad81f1cac13fcc3f0dc7
ER <zhongzsp@aliyun.com>
2018-12-11 11:46:27 +0800

    add open url

--- packages/layout/sub-menu.vue
+++ packages/layout/sub-menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="sk-layout-sub-menu">
+  <div class="sk-layout-sub-menu" :class="{'hidden-sub-menu': menu.length === 0}">
     <div class="title">{{name}}</div>
     <div v-for="(item, index) in menu"
     :key="index"
@@ -26,8 +26,8 @@ export default {
     }
   },
   methods: {
-    openUrl () {
-      
+    openUrl (path) {
+      this.$router.push({ path: path })
     }
   }
 }
