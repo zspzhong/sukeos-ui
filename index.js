8d309f448be6ddd385a616238a1f4b3fa56fccd0
ER <zhongzsp@aliyun.com>
2018-12-16 21:34:55 +0800

    bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Modal from './packages/modal/modal'
 import AddInput from './packages/inputButton/addButton'
-import Prompt from './packages/prompt/prompt.js'
+import SkPrompt from './packages/prompt/prompt.js'
 import Paging from './packages/paging/paging'
 import SkForm from './packages/form/form'
 import SkLayout from './packages/layout/layout'
@@ -15,7 +15,7 @@ const SukeosUi = {
     Vue.component('SkForm', SkForm)
     Vue.component('SkLayout', SkLayout)
     Vue.component('SkTable', SkTable)
-    Vue.prototype.$prompt = Prompt
+    Vue.prototype.$skprompt = SkPrompt
   }
 }
 
--- packages/prompt/prompt.vue
+++ packages/prompt/prompt.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="prompt-page" v-if="show">
+  <div class="sk-prompt" v-if="show">
     <div>
-      <div class="title">{{title}}</div>
-      <div class="describe">{{describe}}</div>
-      <div class="bt-list">
+      <div class="sk-prompt-title">{{title}}</div>
+      <div class="sk-prompt-describe">{{describe}}</div>
+      <div class="sk-prompt-bt-list">
         <div @click="success()">{{btName}}</div>
         <div @click="close()">取消</div>
       </div>
